90392e9154a5f150d03a11baaba2632f1c24ea31
Tylor <tylorsbillings@gmail.com>
2022-08-23 20:06:09 -0500

    Competed caesar cypther project.

--- day8/caesar_cypther.py
+++ day8/caesar_cypther.py
@@ -1,33 +1,37 @@
-from email.headerregistry import ContentTypeHeader
+import art
 
-
+print(art.logo)
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+def caesar(plain_text, shift_amount, user_direction):
+    if user_direction == "encode":
+        encoded_text = ""
+        for letter in plain_text:
+            if letter in alphabet:
+                posistion = alphabet.index(letter)
+                new_posistion = (posistion + shift_amount) % 26
+                new_letter = alphabet[new_posistion]
+                encoded_text += new_letter
+            else: encoded_text += letter 
+        print(f"The encoded text is: {encoded_text}")
+    elif user_direction == "decode":
+        decoded_text = ""
+        for letter in plain_text:
+            if letter in alphabet:
+                posistion = alphabet.index(letter)
+                new_posistion = (posistion - shift_amount) % 26
+                new_letter = alphabet[new_posistion]
+                decoded_text += new_letter
+            else: decoded_text += letter
+        print(f"Your decoded text is: {decoded_text}")
 
-def encrypt(plain_text, shift_amount):
-    encoded_text = ""
-    for letter in plain_text:
-        posistion = alphabet.index(letter)
-        new_posistion = posistion + shift_amount
-        new_letter = alphabet[new_posistion]
-        encoded_text += new_letter
-    print(f"The encoded text is {encoded_text}")
-
-def decrypt(plain_text, shift_amount):
-    decoded_text = ""
-    for letter in plain_text:
-        posistion = alphabet.index(letter)
-        new_posistion = posistion - shift_amount
-        new_letter = alphabet[new_posistion]
-        decoded_text += new_letter
-    print(f"Your decoded text is {decoded_text}")
-
-
-if direction == "encode":
-    encrypt(text, shift)
-elif direction == "decode":
-    decrypt(text, shift)
-else: print(f"Only encrypt and decrypt are options. You chose {direction}")
+should_end = False 
+while not should_end:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    caesar(plain_text=text, shift_amount=shift, user_direction=direction)
+    restart = input("Type yes if you would like to go again. Otherwise type no.").lower()
+    if restart == "no":
+        should_end = True
+        print("Goodbye")
